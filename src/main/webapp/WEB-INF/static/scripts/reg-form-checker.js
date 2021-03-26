754b94b2a7d73db52a271f0f96d72a8bc295423a
vladpolkhovsky <vladpolkhovsky@users.noreply.github.com>
2021-03-26 09:50:32 +0300

    update

--- src/main/webapp/WEB-INF/static/scripts/reg-form-checker.js
+++ src/main/webapp/WEB-INF/static/scripts/reg-form-checker.js
@@ -5,6 +5,26 @@ const password_2 = document.getElementById("password-2");
 
 const span = document.getElementById("password-checker-span");
 
+function checkPasswords() {
+    console.log('\'' + password_1.value + '\'', '\'' + password_2.value + '\'')
+    if (password_1.value != password_2.value || password_1.value === "" || password_2.value === "") {
+        password_1.style.borderColor = password_2.style.borderColor = "#6E00B3";
+        password_1.style.borderWidth = password_2.style.borderWidth = "4px";
+        doRegButton.disabled = true;
+        if (password_1.value != "" || password_2.value != "") {
+            span.innerText = "Пароли не совпадают";
+        } else {
+            span.innerText = "";
+        }
+    }
+    else {
+        span.innerText = "";
+        password_1.style.borderColor = password_2.style.borderColor = "#B3B3B3";
+        password_1.style.borderWidth = password_2.style.borderWidth = "2px";
+        doRegButton.disabled = false
+    }
+}
+
 password_1.addEventListener("keyup", checkPasswords);
 password_2.addEventListener("keyup", checkPasswords);
 
@@ -13,15 +33,4 @@ doRegButton.addEventListener("click", (event) => {
         event.preventDefault();
 })
 
-function checkPasswords() {
-    if (password_1.value != password_2.value) {
-        span.innerText = "Пароли не совпадают";
-        password_1.style.borderColor = password_2.style.borderColor = "#6E00B3";
-        password_1.style.borderWidth = password_2.style.borderWidth = "4px";
-    }
-    else {
-        span.innerText = "";
-        password_1.style.borderColor = password_2.style.borderColor = "#B3B3B3";
-        password_1.style.borderWidth = password_2.style.borderWidth = "2px";
-    }
-}
+checkPasswords();
